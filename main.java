34f7285f9860d6718738c532b69473479e9b1c98
SluttyDevelopment <ilabelrecordsmaill@gmail.com>
2026-03-02 09:55:28 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
        -
-        JOptionPane.showMessageDialog(null, "https://sluttiest.space/", "BEST JAVA/JS STEALER", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null, "https://sluttiest.space/", "JAVA/JS STEALER", JOptionPane.WARNING_MESSAGE);
     }
 }
